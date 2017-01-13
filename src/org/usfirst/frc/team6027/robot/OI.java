b8380722af0e082bfd112edbc7eda6ea91feaaff
Charlie Wade <colson72098@gmail.com>
2017-01-12 23:14:06 -0500

    Move ultrasonic and gyro to OI to allow multiple subsystem access

--- src/org/usfirst/frc/team6027/robot/OI.java
+++ src/org/usfirst/frc/team6027/robot/OI.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team6027.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.usfirst.frc.team6027.robot.commands.DriveDistance;
@@ -7,10 +9,18 @@ import org.usfirst.frc.team6027.robot.commands.DriveDistance;
 public class OI {
 	private Joystick stick = new Joystick(0);
 	private JoystickButton trigger = new JoystickButton(stick,0);
+	private AnalogInput ultrasonic = new AnalogInput(0);
+	private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 	public OI(){
 		trigger.whenActive(new DriveDistance(10));
 	}
 	public Joystick getJoystick(){
 		return stick;
+	}
+	public AnalogInput getUltrasonic(){
+		return ultrasonic;
+	}
+	public ADXRS450_Gyro getGyro(){
+		return gyro;
 	}
 }
