ad5a0dd1df0c6285dcf9faf850ffcfed13888873
Charlie Wade <colson72098@gmail.com>
2017-01-31 09:06:01 -0500

    Update buttons

--- src/org/usfirst/frc/team6027/robot/OI.java
+++ src/org/usfirst/frc/team6027/robot/OI.java
@@ -3,13 +3,16 @@ package org.usfirst.frc.team6027.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.usfirst.frc.team6027.robot.commands.DriveDistance;
+import org.usfirst.frc.team6027.robot.commands.RotateDriveTrain;
 
 public class OI {
 	private Joystick stick = new Joystick(0);
-	private JoystickButton trigger = new JoystickButton(stick,7);
+	private JoystickButton forward = new JoystickButton(stick,7);
+	private JoystickButton backward = new JoystickButton(stick,8);
 
 	public OI(){
-		trigger.whileHeld(new DriveDistance(10));
+		forward.whenPressed(new DriveDistance(40));
+		backward.whenPressed(new RotateDriveTrain(180));
 	}
 
 	public Joystick getJoystick(){
