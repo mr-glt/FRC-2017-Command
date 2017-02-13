c84c7de1408c2cd39b2a2de5e0a073291df2cdb5
Charlie Wade <colson72098@gmail.com>
2017-02-13 08:27:14 -0500

    Map buttons onto Xbox controller

--- src/org/usfirst/frc/team6027/robot/OI.java
+++ src/org/usfirst/frc/team6027/robot/OI.java
@@ -6,16 +6,15 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.usfirst.frc.team6027.robot.commands.*;
 
 public class OI {
-	private Joystick stick = new Joystick(RobotMap.joystickPort);
 	private Joystick controller = new Joystick(RobotMap.controllerPort);
 	private DigitalInput winchLimit = new DigitalInput(RobotMap.winchLimitPort);
 
 	public OI(){
-		JoystickButton x = new JoystickButton(controller, 0);
-		JoystickButton y = new JoystickButton(controller, 1);
-		JoystickButton a = new JoystickButton(controller, 2);
-		JoystickButton b = new JoystickButton(controller, 3);
-		JoystickButton rb = new JoystickButton(controller, 4);
+		JoystickButton x = new JoystickButton(controller, 3);
+		JoystickButton y = new JoystickButton(controller, 4);
+		JoystickButton a = new JoystickButton(controller, 1);
+		JoystickButton b = new JoystickButton(controller, 2);
+		JoystickButton rb = new JoystickButton(controller, 6);
 		JoystickButton lb = new JoystickButton(controller, 5);
 
 		a.toggleWhenPressed(new PickupOn());
@@ -24,9 +23,6 @@ public class OI {
 		x.whenPressed(new Climb());
 		rb.whileHeld(new Shoot());
 
-	}
-	public Joystick getJoystick(){
-		return stick;
 	}
 	public Joystick getController(){
 		return controller;
